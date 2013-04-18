55c80328e1b78e76a101c060defc958f85b42340
Max Bucci <mbucci@bowdoin.edi>
2013-04-18 09:50:16 -0400

    Added TableView For Courses and Games

---
+++ Study With Friends/CoursesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CoursesTableViewController.h
+//  Study With Friends
+//
+//  Created by Max Bucci on 4/16/13.
+//  Copyright (c) 2013 Sharif Younes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoursesTableViewController : UITableViewController
+
+@end
